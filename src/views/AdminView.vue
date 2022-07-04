ba5c7b48b866a2776198e058127d5742576a0c27
Eric HOGEDEZ <erichogedez2@gmail.com>
2022-07-04 10:56:47 +0200

    first commit

---
+++ src/views/AdminView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <header class="entete">
+      <img src="../assets/logo.png" alt="" class="logo" />
+    </header>
+    <div class="container">
+      <admin />
+    </div>
+    <router-link :to="{ path: '/oublie' }"> mot de passe oublié </router-link>
+  </div>
+</template>
+
+<script>
+import AdminComponent from "@/components/AdminComponent.vue";
+const AdminView = {
+  components: {
+    admin: AdminComponent,
+  },
+};
+export default AdminView;
+</script>
+
+<style scoped>
+.entete {
+  min-height: 80px;
+  border-bottom: 4px solid black;
+}
+
+.logo {
+  height: 7vh;
+  margin: 20px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/views/InscriptionView.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <header class="entete">
+      <img src="../assets/logo.png" alt="" class="logo" />
+    </header>
+    <!-- recuperer la variable nom commité -->
+    <h1>Inscrivez-vous dans votre comité</h1>
+    <div class="container">
+      <riverain
+        titre="riverain"
+        :idComiteRiverain="idComite"
+        :showAssoc="show"
+        v-show="visible"
+      />
+      <assoc
+        titre="assoc"
+        :showRiverain="show"
+        :idComiteAssoc="idComite"
+        v-show="!visible"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AssocComponent from "@/components/AssocComponent.vue";
+import RiverainComponent from "@/components/RiverainComponent.vue";
+const InscriptionView = {
+  props: {
+    idComite: Number,
+  },
+  components: {
+    riverain: RiverainComponent,
+    assoc: AssocComponent,
+  },
+  data() {
+    return {
+      visible: true,
+    };
+  },
+  methods: {
+    show() {
+      return (this.visible = !this.visible);
+    },
+  },
+};
+export default InscriptionView;
+</script>
+
+<style scoped>
+.entete {
+  min-height: 80px;
+  border-bottom: 4px solid black;
+}
+
+.logo {
+  height: 7vh;
+  margin: 20px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+}
+</style>
